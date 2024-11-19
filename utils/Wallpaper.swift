b39dc210a07b4895ad98d068c45f4e2dd1b326be
kurama <52566613+KuramaSyu@users.noreply.github.com>
2024-11-19 16:54:50 +0100

    Create Wallpaper.swift

---
+++ utils/Wallpaper.swift
@@ -0,0 +1,32 @@
+//
+//  Wallpaper.swift
+//  DailyPic
+//
+//  Created by Paul Zenker on 19.11.24.
+//
+
+import Foundation
+import SwiftUI
+
+class WallpaperHandler {
+    func setWallpaper(image: URL) {
+        do {
+            let screens = NSScreen.screens
+            for i in screens {
+                print("Changing for screen \(i)")
+                try NSWorkspace.shared.setDesktopImageURL(image, for: i, options: [:])
+            }
+        } catch {
+            print(error)
+        }
+//        let workspace = NSWorkspace()
+//        let screen = NSScreen()
+//        do {
+//            try workspace.setDesktopImageURL(image, for: screen)
+//        } catch {
+//            print("Error setting wallpaper: \(error)")
+//        }
+        
+        
+    }
+}
