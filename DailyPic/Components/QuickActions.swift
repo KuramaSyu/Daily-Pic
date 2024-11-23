9d35e8825285368531e88f986c874bb6f80649e9
kurama <52566613+KuramaSyu@users.noreply.github.com>
2024-11-23 03:59:40 +0100

    chore: cleaned code

--- DailyPic/Components/QuickActions.swift
+++ DailyPic/Components/QuickActions.swift
@@ -15,95 +15,88 @@ struct QuickActions: View {
     @ObservedObject var imageManager: ImageManager
     
     var body: some View {
-        VStack(alignment: .leading) {
-            DisclosureGroup(isExpanded: $isExpanded) {
-                VStack(alignment: .listRowSeparatorLeading) {
-                    
-                    // Refresh Now Button
-                    Button(action: {
-                        Task{ await imageManager.downloadImageOfToday()}
-                    }) { HStack {
-                            Image(systemName: "icloud.and.arrow.down")
-                                .font(.title2)
-                            Text("Refresh Now")
-                                .font(.body)
-                                .scaledToFill()
-                        }
-                        .frame(maxWidth: .infinity)
+        DisclosureGroup(isExpanded: $isExpanded) {
+            VStack(alignment: .center) {
+                
+                // Refresh Now Button
+                Button(action: {
+                    Task{ await imageManager.downloadImageOfToday()}
+                }) { HStack {
+                        Image(systemName: "icloud.and.arrow.down")
+                            .font(.title2)
+                        Text("Refresh Now")
+                            .font(.body)
                     }
-                    .buttonStyle(.borderless)
-                    .padding(1)
-                    .hoverEffect()
-                    
-                    // Wallpaper Button
-                    Button(action: {
-                        if let url = imageManager.currentImageUrl {
-                            WallpaperHandler().setWallpaper(image: url)
-                        }
-                    }) { HStack {
-                            Image(systemName: "photo.tv")
-                                .font(.title2)
-                            Text("Set as Wallpaper")
-                                .font(.body)
-                                .scaledToFill()
-                        }
-                        .frame(maxWidth: .infinity)
-                    }
-                    .buttonStyle(.borderless)
-                    .padding(1)
-                    .hoverEffect()
-                    
-                    // Open Folder
-                    Button(action: {imageManager.openFolder()}) {
-                        HStack {
-                            Image(systemName: "folder.fill")
-                                .font(.title2)
-                            Text("Open Folder")
-                                .font(.body)
-                                .scaledToFill()
-                        }
-                        .frame(maxWidth: .infinity)
-                    }
-                    .buttonStyle(.borderless)
-                    .padding(1)
-                    .hoverEffect()
-                    
-                    // Exit App
-                    Button(action: {
-                        NSApplication.shared.terminate(nil) // Shuts down the app
-                    }) {
-                        HStack {
-                            Image(systemName: "rectangle.portrait.and.arrow.right")
-                                .font(.title2)
-                            Text("Quit")
-                                .font(.body)
-                        }
-                        .frame(maxWidth: .infinity)
-                        .padding(.vertical, 1)
-
-                            
-                    }
-                    //.background(Color.red.opacity(0.2))
-                    //.cornerRadius(8)
-                    .buttonStyle(.borderless)
-                    .padding(.horizontal, 40)
-                    .hoverEffect()
-                    
-                    
                 }
-                .padding(.leading, 2) // Optional, for visual hierarchy
-            } label: {
-                Text("Quick Actions")
-                    .font(.headline)
-                    .padding(6)
-                    //.foregroundColor(.blue)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 40)
+                .buttonStyle(.borderless)
+                .padding(1)
+                .hoverEffect()
+                
+                // Wallpaper Button
+                Button(action: {
+                    if let url = imageManager.currentImageUrl {
+                        WallpaperHandler().setWallpaper(image: url)
+                    }
+                }) { HStack {
+                        Image(systemName: "photo.tv")
+                            .font(.title2)
+                        Text("Set as Wallpaper")
+                            .font(.body)
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 40)
+                .buttonStyle(.borderless)
+                .padding(1)
+                .hoverEffect()
+                
+                // Open Folder
+                Button(action: {imageManager.openFolder()}) {
+                    HStack {
+                        Image(systemName: "folder.fill")
+                            .font(.title2)
+                        Text("Open Folder")
+                            .font(.body)
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 40)
+                .buttonStyle(.borderless)
+                .padding(1)
+                .hoverEffect()
+                
+                // Exit App
+                Button(action: {
+                    NSApplication.shared.terminate(nil) // Shuts down the app
+                }) { HStack {
+                        Image(systemName: "rectangle.portrait.and.arrow.right")
+                            .font(.title2)
+                        Text("Quit")
+                            .font(.body)
+                    }
+                
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 40)
+                .buttonStyle(.borderless)
+                .padding(1)
+                .hoverEffect()
             }
-            .padding(6)
-            .background(Color.gray.opacity(0.2))
-            .cornerRadius(8)
-            
+        } label: {
+            Text("Quick Actions")
+                .font(.headline)
+                .padding(.leading, 6)
+                .padding(2)
         }
-        .padding()
+        .padding(.vertical, 6)  // padding from last toggle to bottom
+        .padding(.horizontal, 10)  // padding at left for > and for buttons
+        .background(Color.gray.opacity(0.2))
+        .cornerRadius(8)
+        .contentShape(Rectangle()) // Makes the entire label tappable
+        .onTapGesture {
+            withAnimation { isExpanded.toggle() }
+        }
     }
-    
 }
