55f62306c8ab94b83635fa977f80341373f4a22b
kurama <paul.zenker.pz@gmail.com>
2025-05-16 10:06:30 +0200

    Create WallpaperApiProtocol.swift

---
+++ utils/api/WallpaperApiProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  WallpaperApiProtocol.swift
+//  Daily Pic
+//
+//  Created by Paul Zenker on 16.05.25.
+//
+
+import Foundation
+
+public protocol WallpaperApiProtocol {
+    func downloadImage(of date: Date) async -> Response?
+}
