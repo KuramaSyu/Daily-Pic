8be417f9a7f8be296472e331c36d78e430d2750d
kurama <paul.zenker.pz@gmail.com>
2025-05-21 12:24:41 +0200

    api: added init() to protocol

--- Daily Pic/api/NamedImageProtocol.swift
+++ Daily Pic/api/NamedImageProtocol.swift
@@ -7,6 +7,7 @@
 import AppKit
 
 public protocol NamedImageProtocol: Hashable {
+    init(url: URL, creation_date: Date, image: NSImage?)
     func unloadImage();
     func exists() -> Bool;
     func getTitle() -> String;
