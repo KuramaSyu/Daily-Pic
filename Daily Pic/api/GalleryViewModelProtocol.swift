1bd76e91d54e1b15a0636f95e04ca336b5547074
kurama <paul.zenker.pz@gmail.com>
2025-05-22 10:41:29 +0200

    api: added protocol for GammeryViewModel

---
+++ Daily Pic/api/GalleryViewModelProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  GalleryViewModelProtocol.swift
+//  Daily Pic
+//
+//  Created by Paul Zenker on 22.05.25.
+//
+
+public protocol GalleryViewModelProtocol {
+    associatedtype imageType: NamedImageProtocol
+    associatedtype galleryType: GalleryModelProtocol
+    static var shared: Self { get }
+    var image: imageType? { get set }
+    var favoriteImages: Set<imageType> { get set }
+    var galleryModel: galleryType { get }
+}
