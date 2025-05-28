e78da5d7734e3fcf30d496c95c096aca6d8d1368
kurama <paul.zenker.pz@gmail.com>
2025-05-28 19:31:23 +0200

    Create OsuWallpaperApi.swift

---
+++ Daily Pic/impl/OsuWallpaperApi.swift
@@ -0,0 +1,16 @@
+//
+//  OsuWallpaperApi.swift
+//  Daily Pic
+//
+//  Created by Paul Zenker on 28.05.25.
+//
+
+import Foundation
+
+class OsuWallpaperApi: WallpaperApiProtocol {
+    func downloadImage(of date: Date) async -> Response? {
+        return nil
+    }
+    
+    
+}
