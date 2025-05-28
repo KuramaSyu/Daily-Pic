724dbc9365029633c44d056b2941a3511fdba72f
kurama <paul.zenker.pz@gmail.com>
2025-05-28 19:31:11 +0200

    Create WallpaperAdapterProtocol.swift

---
+++ Daily Pic/api/WallpaperAdapterProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  WallpaperAdapterProtocol.swift
+//  Daily Pic
+//
+//  Created by Paul Zenker on 28.05.25.
+//
+import Foundation
+
+public protocol WallpaperMarket {
+    var images: [WallpaperProtocol] {get set}
+}
+
+/// represents the protocol for classes which represent one specific
+/// wallpaper from one specific market
+public protocol WallpaperProtocol {
+    func saveFile() async throws
+    func getImageURL() -> URL
+    func getImageName() -> String
+    func getJsonName() -> String
+}
