71d97c63708cd6f9f4317c57b5d4953d20bf94ed
kurama <paul.zenker.pz@gmail.com>
2025-08-28 17:30:20 +0200

    refactor, feat: use of MenuContext to switch between ViewModels

--- Daily Pic/DailyPicApp.swift
+++ Daily Pic/DailyPicApp.swift
@@ -19,11 +19,12 @@ import ImageIO
 struct DailyPicApp: App {
     // 2 variables to set default focus https://developer.apple.com/documentation/swiftui/view/prefersdefaultfocus(_:in:)
     
-    @ObservedObject var galleryView = BingGalleryViewModel.shared
     @Namespace var mainNamespace
     @Environment(\.resetFocus) var resetFocus
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @State private var api: WallpaperApiEnum = .bing
+    @ObservedObject var galleryView = BingGalleryViewModel.shared
+
 
     let menuIcon: NSImage = {
         let ratio = $0.size.height / $0.size.width
@@ -34,104 +35,15 @@ struct DailyPicApp: App {
     
     var body: some Scene {
         MenuBarExtra() {
-            // Title
-            ZStack {
-                // title
-                VStack {
-                    Text(self.getTitleText())
-                        .font(.headline)
-                        .padding(.top, 15)
-                    if let title = galleryView.currentImage?.getTitle() {
-                        Text(title)
-                            .font(.subheadline)
-                    }
-                }
-                
-                // Top-right gear button overlay
-                VStack {
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            SettingsWindowController.shared.showSettings()
-                        }) {
-                            Image(systemName: "gearshape")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 20, height: 20)
-                                .padding(6)
-                        }
-                        .background(Color.gray.opacity(0.2))
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
-                        .buttonStyle(PlainButtonStyle()) // Prevents default macOS blue highlight
-                    }
-                    .padding(6)
-                    Spacer()
-                }
-            }
-
-            
-            if let nextImage = galleryView.revealNextImage {
-                RevealNextImageView(revealNextImage: nextImage)
-                    .transition(.opacity.combined(with: .scale)) // Add transition effect
-                    .animation(.easeInOut(duration: 0.8), value: (galleryView.revealNextImage != nil))
-            }            // Image Data
-            VStack() {
-                if let current_image = galleryView.currentImage {
-                    DropdownWithToggles(
-                        bingImage: current_image.metadata, image: current_image,
-                        imageManager: galleryView
-                    )
-                    // Image Preview
-                    if let loaded_image = current_image.loadNSImage() {
-                        Image(nsImage: loaded_image)
-                            .resizable()
-                            .scaledToFit()
-                            .cornerRadius(20)
-                            .shadow(radius: 3)
-                            // Adding a tap gesture to open the image in an image viewer
-                            .onTapGesture {
-                                openInViewer(url: current_image.url)
-                            }
-                    }
-                } else {
-                    VStack(alignment: .center) {
-                        Image(systemName: "arrow.trianglehead.2.clockwise.rotate.90.icloud")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(minWidth: 50, minHeight: 50)
-                            .padding(.top, 10)
-                        Text("No image available.")
-                            .font(.headline)
-                            .padding(10)
-                        Text("Downloading images from last 7 days...")
-                            .font(.headline)
-                            .padding(10)
-                    }
-                    .scaledToFit()
-                    .frame(maxWidth: .infinity, minHeight: 100, maxHeight: 200, alignment: .center)
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
-                }
-                // add image navigation buttons
-                ImageNavigation(imageManager: galleryView)
-                    .scaledToFit()  // make it not overflow the box
-                // add api selection
-                ApiSelection(selectedApi: self.$api)
-                    .scaledToFit()
-                // add quick action menu
-                QuickActions(imageManager: galleryView)
-                    .layoutPriority(2)
-                    .padding(.bottom, 10)
-            }
-            .padding(.horizontal, 15)
-            .frame(width: 350, height: 450)
-            .focusScope(mainNamespace)
-            .onAppear {
-                Task {await self.galleryView.imageTracker.downloadMissingImages(from: nil, reloadImages: true)}
-            }
-            .focusEffectDisabled(true)
-            .onDisappear {
-                galleryView.onDisappear();
+            switch api {
+            case .bing:
+                MenuContent (
+                    vm: BingGalleryViewModel.shared, api: $api, menuIcon: menuIcon
+                )
+            case .osu:
+                MenuContent (
+                    vm: OsuGalleryViewModel.shared, api: $api, menuIcon: menuIcon
+                )
             }
         } label: {
             Image(nsImage: menuIcon)
@@ -139,15 +51,7 @@ struct DailyPicApp: App {
         .menuBarExtraStyle(.window)
     }
     
-    func getTitleText() -> String {
-        let wrap_text = { (date: String) in return "Picture of \(date)" }
-        
-        guard let image = galleryView.currentImage else {
-            // use DateParser for date formatting
-            return wrap_text(DateParser.prettyDate(for: Date()))
-        }
-        return image.getSubtitle();
-    }
+
     
     private func openInViewer(url: URL) {
         NSWorkspace.shared.open(url)
