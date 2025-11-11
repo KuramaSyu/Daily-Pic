a717fddc8730248a4f136cdfc5aa24d282cc0e19
kurama <paul.zenker.pz@gmail.com>
2025-11-11 12:28:58 +0100

    docs: added docs to main view

--- Daily Pic/impl/MenuContext.swift
+++ Daily Pic/impl/MenuContext.swift
@@ -10,6 +10,7 @@ struct MenuContent<VM: GalleryViewModelProtocol, IM: ImageTrackerProtocol>: View
 
     var body: some View {
         ZStack {
+            // title
             VStack {
                 Text(getTitleText())
                     .font(.headline)
@@ -19,6 +20,7 @@ struct MenuContent<VM: GalleryViewModelProtocol, IM: ImageTrackerProtocol>: View
                 }
             }
 
+            // settings icon on top right
             VStack {
                 HStack {
                     Spacer()
@@ -39,13 +41,18 @@ struct MenuContent<VM: GalleryViewModelProtocol, IM: ImageTrackerProtocol>: View
             }
         }
 
+        // optional image reveal message
         if let nextImage = vm.revealNextImage {
             RevealNextImageView(revealNextImage: nextImage, vm: self.vm)
                 .transition(.opacity.combined(with: .scale))
                 .animation(.easeInOut(duration: 0.8), value: (vm.revealNextImage != nil))
         }
 
+        // the image or DL-screen if no image is there
+        // and the image navigation
+        // and quick action drop down
         VStack {
+            // image display
             if let currentImage = vm.currentImage {
                 DropdownWithToggles(
                     image: currentImage,
@@ -60,6 +67,7 @@ struct MenuContent<VM: GalleryViewModelProtocol, IM: ImageTrackerProtocol>: View
                         .onTapGesture { openInViewer(url: currentImage.url) }
                 }
             } else {
+                // DL-display
                 VStack(alignment: .center) {
                     Image(systemName: "arrow.trianglehead.2.clockwise.rotate.90.icloud")
                         .resizable()
@@ -75,8 +83,10 @@ struct MenuContent<VM: GalleryViewModelProtocol, IM: ImageTrackerProtocol>: View
                 .cornerRadius(8)
             }
 
+            // image navigation buttons
             ImageNavigation(imageManager: vm).scaledToFit()
-                   
+            
+            // quick action menu
             QuickActions(imageManager: vm, imageTracker: imageTracker, api: $api)
                 .layoutPriority(2)
                 .padding(.bottom, 10)
